dc2513087f22688dd8823afd104bf6ddc84b5f37
mohamed7911 <mdalaa7911@gmail.com>
2023-04-23 03:43:48 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const date = require(__dirname+'/date.js')
+
+const items = ['Buy food','Cook food','Eat food']
+const workItems = []
+
+
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    let day = date.getDate()
+    res.render('list',{listTitle:day,newListItems:items})
+})
+
+
+app.post('/',(req,res)=>{
+    let item = req.body.newItem
+    if(req.body.list === 'Work'){
+        workItems.push(item)
+        res.redirect('/work')
+    }else{
+        items.push(item)
+        res.redirect('/')
+    }
+})
+
+app.get('/work',(req,res)=>{
+    res.render('list',{listTitle:'Work List',newListItems:workItems})
+})
+
+app.post('/work',(req,res)=>{
+    let item = req.body.newItem
+    workItems.push(item)
+    res.redirect('/work')
+})
+
+app.listen(3000)
---
+++ date.js
@@ -0,0 +1,10 @@
+module.exports.getDate = ()=>{
+    const today = new Date()
+    const options = {
+        weekday: 'long',
+        day:'numeric',
+        month: 'long'
+    }
+    return today.toLocaleDateString('en-US',options)
+
+}
